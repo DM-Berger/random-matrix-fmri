ce22a3f95c3c4419dfb1db338e7b5d8db9c9ff3a
Derek M Berger <dmberger.dev@gmail.com>
2022-10-07 12:01:10 -0400

    update configs and install

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PYTHON="$PYENV_ROOT/versions/3.10.4/bin/python"
+$PYTHON --version || exit 1
+$PYTHON -m pip install --upgrade pip
+$PYTHON -m pip install empyricalRMT==1.1.1
+$PYTHON -m pip install matplotlib numpy pandas typing_extensions tqdm seaborn statsmodels scikit-learn scipy nibabel pytest mypy isort flake8
