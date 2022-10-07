e55c64d64805ddd53cb305ef78d7702a9809e520
Derek M Berger <dmberger.dev@gmail.com>
2022-10-07 12:04:42 -0400

    derp

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 PYTHON="$PYENV_ROOT/versions/3.10.4/bin/python"
 $PYTHON --version || exit 1
 $PYTHON -m pip install --upgrade pip
+$PYTHON -m venv .venv
+
+PYTHON=".venv/bin/python"
 $PYTHON -m pip install empyricalRMT==1.1.1
 $PYTHON -m pip install matplotlib numpy pandas typing_extensions tqdm seaborn statsmodels scikit-learn scipy nibabel pytest mypy isort flake8
