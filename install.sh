11f2108e5e283708cd8137c61d983609bf26c29d
Derek M Berger <dmberger.dev@gmail.com>
2022-10-07 12:06:26 -0400

    improve install

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ PYTHON="$PYENV_ROOT/versions/3.10.4/bin/python"
 $PYTHON --version || exit 1
 $PYTHON -m pip install --upgrade pip
 $PYTHON -m venv .venv
+echo "Created virtual environment in .venv"
 
 PYTHON=".venv/bin/python"
+$PYTHON -m pip install --upgrade pip
 $PYTHON -m pip install empyricalRMT==1.1.1
 $PYTHON -m pip install matplotlib numpy pandas typing_extensions tqdm seaborn statsmodels scikit-learn scipy nibabel pytest mypy isort flake8
