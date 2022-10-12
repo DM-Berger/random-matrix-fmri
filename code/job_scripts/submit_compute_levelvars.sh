ac061ca210bb77eea5f9d6084eb12ba94d170ce7
Derek M Berger <dmberger.dev@gmail.com>
2022-10-12 10:47:13 -0400

    write job scripts

---
+++ code/job_scripts/submit_compute_levelvars.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=def-jlevman
+#SBATCH --mail-user=dberger@stfx.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=TIME_LIMIT_90
+#SBATCH --profile=all
+#SBATCH --job-name=all_levelvar
+#SBATCH --output=all_levelvars_%j.out
+#SBATCH --time=00-24:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=80
+
+THIS_SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+CODE="$(dirname "$THIS_SCRIPT_DIR")"
+PROJECT="$(dirname "$CODE")"
+JOB_SCRIPTS="$CODE/job_scripts"
+RUN_SCRIPT="$PROJECT/run_python.sh"
+
+PY_SCRIPTS="$CODE/scripts"
+PY_SCRIPT="$(readlink -f "$PY_SCRIPTS/compute_levelvars.py")"
+
+bash "$RUN_SCRIPT" "$PY_SCRIPT"
---
+++ code/scripts/compute_levelvars.py
@@ -0,0 +1,16 @@
+# fmt: off
+import sys  # isort: skip
+from pathlib import Path  # isort: skip
+ROOT = Path(__file__).resolve().parent.parent
+sys.path.append(str(ROOT))
+# fmt: on
+
+from rmt.dataset import ProcessedDataset, levelvars, rigidities
+from rmt.enumerables import Dataset
+
+if __name__ == "__main__":
+    for source in Dataset:
+        for preproc in [True, False]:
+            for degree in [3, 5, 7, 9]:
+                data = ProcessedDataset(source=source, full_pre=preproc)
+                level_vars = levelvars(dataset=data, degree=degree, parallel=True)
