85e08d2767b29c13c54dec241c85e001a6338132
Derek M Berger <dmberger.dev@gmail.com>
2022-10-24 14:43:43 -0400

    [WIP]

--- code/scripts/summarize_everything.py
+++ code/scripts/summarize_everything.py
@@ -18,6 +18,7 @@ from matplotlib.figure import Figure
 from matplotlib.patches import Patch
 from pandas import DataFrame
 from pandas.errors import PerformanceWarning
+from seaborn import FacetGrid
 from tqdm import tqdm
 
 from rmt.features import FEATURE_OUTFILES as PATHS
@@ -35,6 +36,7 @@ DROPS = [
 ]
 BLUE = "#004cc7"
 ORNG = "#f68a0e"
+GREEN = "#01f91e"
 GREY = "#5c5c5c"
 BLCK = "#000000"
 PURP = "#8e02c5"
@@ -61,6 +63,43 @@ SUBGROUPERS = [
     ["data", "comparison", "classifier", "preproc", "deg", "trim"],
     ["data", "comparison", "classifier", "preproc", "deg", "norm"],
     ["data", "comparison", "classifier", "preproc", "deg", "trim", "norm"],
+]
+
+FEATURE_GROUP_PALETTE = {
+    "eigs": BLCK,
+    "eigs max": ORNG,
+    "eigs smooth": GREEN,
+    "eigs middle": GREY,
+    "rmt + eigs": PURP,
+    "rmt only": BLUE,
+}
+
+SLICE_ORDER = [
+    "all",
+    "max-25",
+    "max-10",
+    "max-05",
+    "mid-25",
+    "mid-10",
+    "mid-05",
+    "min-25",
+    "min-10",
+    "min-05",
+]
+
+SUBGROUP_ORDER = [
+    "Vigilance - high v low",
+    "TaskAttention - high v low",
+    "WeeklyAttention - high v low",
+    "Osteo - allpain v nopain",
+    "Osteo - allpain v pain",
+    "Osteo - allpain v duloxetine",
+    "Osteo - nopain v duloxetine",
+    "Osteo - duloxetine v pain",
+    "Osteo - nopain v pain",
+    "Parkinsons - control v parkinsons",
+    "Parkinsons - control_pre v park_pre",
+    "Learning - task v rest",
 ]
 
 
@@ -237,6 +276,9 @@ def make_legend(fig: Figure, position: str | tuple[float, float] = "upper right"
 def summarize_performance_by_aggregation(
     metric: Literal["auroc", "f1", "acc+"], summarizer: Literal["median", "best"]
 ) -> None:
+    ax: Axes
+    fig: Figure
+
     def resize_fig() -> None:
         fig = plt.gcf()
         fig.set_size_inches(w=40, h=26)
@@ -251,88 +293,191 @@ def summarize_performance_by_aggregation(
     df = df.loc[~df.data.str.contains("Reflect")]
     df = df.loc[~df.data.str.contains("Ses")]
 
-    # not bad view of features overall
-    sbn.displot(
-        data=df,
-        x="auroc",
-        hue="feature_group",
-        col="subgroup",
-        col_wrap=6,
-        element="step",
-        facet_kws=dict(ylim=(0.0, 0.5)),
-        stat="density",
-    )
-    plt.show()
-
     # cleaner view
+    """
     sbn.displot(
         data=df,
         x="auroc",
         kind="kde",
         hue="feature_group",
+        palette=FEATURE_GROUP_PALETTE,
+        hue_order=list(FEATURE_GROUP_PALETTE.keys()),
         col="subgroup",
         col_wrap=6,
         facet_kws=dict(ylim=(0.0, 0.5)),
     )
+    fig = plt.gcf()
+    for i, ax in enumerate(fig.axes):
+        ymin, ymax = ax.get_ylim()
+        ax.vlines(
+            x=0.5,
+            ymin=ymin,
+            ymax=ymax,
+            colors=["black"],
+            linestyles="dashed",
+            alpha=0.7,
+            label="guess" if i == 0 else None,
+        )
     plt.show()
+    """
 
     # violin plot
-    sbn.catplot(data=df, y="auroc", x="subgroup", hue="feature_group", kind="violin")
+    """
+    sbn.catplot(
+        data=df,
+        y="auroc",
+        x="subgroup",
+        hue="feature_group",
+        palette=FEATURE_GROUP_PALETTE,
+        hue_order=list(FEATURE_GROUP_PALETTE.keys()),
+        kind="violin",
+        linewidth=1.0,
+    )
+    fig, ax = plt.gcf(), plt.gca()
+    fig.suptitle("Feature Distributions")
+    for ax in fig.axes:
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
+        xmin, xmax = ax.get_xlim()
+        ax.hlines(
+            y=0.5,
+            xmin=xmin,
+            xmax=xmax,
+            colors=["black"],
+            linestyles="dashed",
+        )
     resize_fig()
     plt.show()
+    """
 
     # Very useful: shows best RMT performance due to outlier performances
+    """
     sbn.catplot(
-        data=df, y="auroc", x="subgroup", hue="feature_group", kind="box", col="preproc"
+        data=df,
+        y="auroc",
+        x="subgroup",
+        hue="feature_group",
+        palette=FEATURE_GROUP_PALETTE,
+        hue_order=list(FEATURE_GROUP_PALETTE.keys()),
+        kind="box",
+        col="preproc",
+        linewidth=0.5,
+        legend_out=False,
     )
+    fig = plt.gcf()
+    # sbn.move_legend(fig, loc="upper right")
+    for ax in fig.axes:
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
     resize_fig()
     plt.show()
+    """
 
     # Also useful if you re-order hue and cols to show max_eigs most important
-    sbn.catplot(
-        data=df, y="auroc", x="slice", hue="feature_group", kind="box", col="preproc"
-    )
-    resize_fig()
-    plt.show()
-
-    # This is important: Shows clear interaction between preproc and slice
+    """
     sbn.catplot(
         data=df,
         y="auroc",
         x="slice",
+        order=SLICE_ORDER,
         hue="feature_group",
+        palette=FEATURE_GROUP_PALETTE,
+        hue_order=list(FEATURE_GROUP_PALETTE.keys()),
         kind="box",
-        row="preproc",
-        col="subgroup",
+        col="preproc",
+        legend_out=False,
+        linewidth=0.5,
     )
     resize_fig()
     plt.show()
+    """
+
+    # This is important: Shows clear interaction between preproc and slice
+    """
+    sbn.catplot(
+        data=df,
+        x="auroc",
+        y="slice",
+        order=SLICE_ORDER,
+        hue="feature_group",
+        palette=FEATURE_GROUP_PALETTE,
+        hue_order=list(FEATURE_GROUP_PALETTE.keys()),
+        kind="box",
+        row="preproc",
+        col="subgroup",
+        linewidth=0.25,
+        fliersize=0.5,  # outlier markers
+    )
+    fig = plt.gcf()
+    for ax in fig.axes:
+        axtitle = ax.get_title()
+        ax.set_title(
+            axtitle[axtitle.find("|") + 1 :]
+            .replace("subgroup = ", "")
+            .replace(" - ", "\n"),
+            fontsize=10,
+        )
+    resize_fig()
+    fig.subplots_adjust(top=0.93, right=0.93)
+    fig.text(x=0.45, y=0.5, s="Minimal preprocessing")
+    fig.text(x=0.45, y=0.98, s="More preprocessing")
+    plt.show()
+    """
 
     # This shows preproc / slice / feature interaction even more clearly than above
+    """
     sbn.catplot(
         data=df,
         x="auroc",
         col="subgroup",
+        col_order=SUBGROUP_ORDER,
         y="feature_group",
         kind="box",
         hue="slice",
+        hue_order=SLICE_ORDER,
         row="preproc",
+        linewidth=0.25,
+        fliersize=0.5,
     )
+    fig = plt.gcf()
+    for ax in fig.axes:
+        axtitle = ax.get_title()
+        ax.set_title(
+            axtitle[axtitle.find("|") + 1 :]
+            .replace("subgroup = ", "")
+            .replace(" - ", "\n"),
+            fontsize=10,
+        )
     resize_fig()
+    fig.subplots_adjust(top=0.93, right=0.93, left=0.055)
+    fig.text(x=0.45, y=0.5, s="Minimal preprocessing")
+    fig.text(x=0.45, y=0.98, s="More preprocessing")
     plt.show()
+    """
 
     # This one very clearly shows effect of preprocessing
-    sbn.catplot(
+    grid: FacetGrid = sbn.catplot(
         data=df,
         y="auroc",
         col="subgroup",
+        col_order=SUBGROUP_ORDER,
         col_wrap=4,
         x="feature_group",
+        order=list(FEATURE_GROUP_PALETTE.keys()),
         kind="box",
         hue="preproc",
+        linewidth=0.5,
+        fliersize=0.75,
     )
+    fig = plt.gcf()
+    fig.suptitle("Effect of Preprocessing by Subgroup")
+    for ax in fig.axes:
+        axtitle = ax.get_title()
+        ax.set_title(axtitle.replace("subgroup = ", ""), fontsize=10)
+        plt.setp(ax.get_xticklabels(), rotation=40, ha="right")
+    grid.set_axis_labels(rotation=30)
     resize_fig()
+    fig.subplots_adjust(top=0.93, right=0.945, bottom=0.105)
     plt.show()
+    sys.exit()
 
     # The more levels we include, the less we "generalize" our claims
     for grouper, aggregates in zip(SUBGROUPERS, AGGREGATES):
