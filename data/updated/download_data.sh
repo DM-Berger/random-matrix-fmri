a6da01c09b49d70319e07e7034144552d745ec95
Derek M Berger <dmberger.dev@gmail.com>
2022-10-25 18:21:18 -0400

    add download script

---
+++ data/updated/download_data.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+DATA="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+cd "$DATA" || exit 1
+
+mkdir -p Park_v_Control
+cd Park_v_Control || exit 1
+aws s3 sync --no-sign-request s3://openneuro.org/ds001907 ds001907-download/
+
+mkdir -p Rest_v_LearningRecall
+cd Rest_v_LearningRecall || exit 1
+aws s3 sync --no-sign-request s3://openneuro.org/ds001454 ds001454-download/
+
+mkdir -p Rest_w_Depression_v_Control
+cd Rest_w_Depression_v_Control || exit 1
+aws s3 sync --no-sign-request s3://openneuro.org/ds002748 ds002748-download/
+
+mkdir -p Rest_w_Healthy_v_OsteoPain
+cd Rest_w_Healthy_v_OsteoPain || exit 1
+aws s3 sync --no-sign-request s3://openneuro.org/ds000208 ds000208-download/
+
+mkdir -p Rest_w_VigilanceAttention
+cd Rest_w_VigilanceAttention || exit 1
+aws s3 sync --no-sign-request s3://openneuro.org/ds001168 ds001168-download/
+
