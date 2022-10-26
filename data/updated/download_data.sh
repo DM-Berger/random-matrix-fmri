67846d58d3f704632c9c14cce931d1d02e33861b
Derek Berger <dberger@stfx.ca>
2022-10-25 20:11:24 -0400

    fix download script

--- data/updated/download_data.sh
+++ data/updated/download_data.sh
@@ -7,20 +7,25 @@ $AWS --version || echo "Something went wrong. Make sure you have aws-cli install
 mkdir -p Park_v_Control
 cd Park_v_Control || exit 1
 $AWS s3 sync --no-sign-request s3://openneuro.org/ds001907 ds001907-download/
+cd "$DATA" || exit 1
 
 mkdir -p Rest_v_LearningRecall
 cd Rest_v_LearningRecall || exit 1
 $AWS s3 sync --no-sign-request s3://openneuro.org/ds001454 ds001454-download/
+cd "$DATA" || exit 1
 
 mkdir -p Rest_w_Depression_v_Control
 cd Rest_w_Depression_v_Control || exit 1
 $AWS s3 sync --no-sign-request s3://openneuro.org/ds002748 ds002748-download/
+cd "$DATA" || exit 1
 
 mkdir -p Rest_w_Healthy_v_OsteoPain
 cd Rest_w_Healthy_v_OsteoPain || exit 1
 $AWS s3 sync --no-sign-request s3://openneuro.org/ds000208 ds000208-download/
+cd "$DATA" || exit 1
 
 mkdir -p Rest_w_VigilanceAttention
 cd Rest_w_VigilanceAttention || exit 1
 $AWS s3 sync --no-sign-request s3://openneuro.org/ds001168 ds001168-download/
+cd "$DATA" || exit 1
 
