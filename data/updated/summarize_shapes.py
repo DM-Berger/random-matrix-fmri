bf70ac5ab45f91a471c8c365d42198132cff88a9
Derek M Berger <dmberger.dev@gmail.com>
2022-10-26 22:24:03 -0400

    [WIP]

--- data/updated/summarize_shapes.py
+++ data/updated/summarize_shapes.py
@@ -4,31 +4,38 @@ import pandas as pd
 from ants import ANTsImage, image_read
 from pandas import DataFrame
 from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
 
 DATA = Path(__file__).resolve().parent
 DATASETS = sorted(filter(lambda p: p.is_dir(), Path(".").glob("*")))
+
+
+def get_fmri_shapes(fmri: Path) -> DataFrame:
+    img: ANTsImage = image_read(fmri)
+    x, y, z, t = img.shape
+    xx, yy, zz, TR = img.spacing
+    return DataFrame(
+        {
+            "data": data.name,
+            "x_n": x,
+            "y_n": y,
+            "z_n": z,
+            "t": t,
+            "x_mm": xx,
+            "y_mm": yy,
+            "z_mm": zz,
+            "TR": TR,
+        },
+        index=[0],
+    )
+
 
 if __name__ == "__main__":
     dfs = []
     for data in DATASETS:
         fmris = sorted(data.rglob("*bold.nii.gz"))
         print(f"Collecting shape info for {data.name}")
-        for fmri in tqdm(fmris):
-            img: ANTsImage = image_read(fmri)
-            x, y, z, t = img.shape
-            xx, yy, zz, TR = img.spacing
-            dfs.append(DataFrame({
-                "data": data.name,
-                "x_n": x,
-                "y_n": y,
-                "z_n": z,
-                "t": t,
-                "x_mm": xx,
-                "y_mm": yy,
-                "z_mm": zz,
-                "TR": TR,
-
-            }, index=[0]))
+        dfs.extend(process_map(get_fmri_shapes, fmris))
 
     df = pd.concat(dfs, axis=0, ignore_index=True)
     out = DATA / "shape_summary.csv"
