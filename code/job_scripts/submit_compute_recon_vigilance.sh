7b1d1c64522eae2695171d57cee2df351696104b
Derek M Berger <dmberger.dev@gmail.com>
2022-10-28 16:39:54 -0400

    add recon-all script

---
+++ code/job_scripts/submit_compute_recon_vigilance.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --account=def-jlevman
+#SBATCH --mail-user=dberger@stfx.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=TIME_LIMIT_90
+#SBATCH --profile=all
+#SBATCH --job-name=all_obs
+#SBATCH --output=freesurf_%A_%a_%j.out
+#SBATCH --array=1-22
+#SBATCH --time=00-24:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=80
+
+ID=$(seq -f "%02g" "$SLURM_ARRAY_TASK_ID")
+
+SCRATCH="$(readlink -f "$SCRATCH")"
+
+PROJECT="$SCRATCH/random-matrix-fmri"
+FILE="$PROJECT/data/updated/Rest_w_VigilanceAttention/ds001168-download/sub-$ID/ses-1/anat/sub-$ID""_ses-1_T1w.nii.gz"
+OUTDIR="$PROJECT/data/updated/Rest_w_VigilanceAttention/ds001168-download/sub-$ID/ses-1/anat"
+export SUBJECTS_DIR="$OUTDIR/freesurfer"
+
+recon-all -subject "sub-$ID""_ses-1" -i "$FILE"
