7a49dc97e2fe8050c88ab06ee474d7113b2785e4
Derek M Berger <dmberger.dev@gmail.com>
2022-10-28 17:16:56 -0400

    [SHARE]

--- code/preprocess/preprocess.py
+++ code/preprocess/preprocess.py
@@ -810,7 +810,7 @@ if __name__ == "__main__":
     paths = get_fmri_paths(filt="Learning")
     # paths = get_fmri_paths()
     # process_map(make_slicetime_file, paths, chunksize=1)
-    # process_map(brain_extract_parallel, paths, chunksize=1)
+    process_map(brain_extract_parallel, paths, chunksize=1)
     # process_map(inspect_extractions, paths, chunksize=1, max_workers=40)
     # process_map(anat_extract_parallel, paths, chunksize=1, max_workers=40)
     # process_map(inspect_extractions, paths, chunksize=1, max_workers=40)
