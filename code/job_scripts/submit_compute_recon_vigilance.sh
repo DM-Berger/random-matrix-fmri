5c0549e9cd8d6de1b80c82c31a39dd81716b5b62
Derek M Berger <dmberger.dev@gmail.com>
2022-10-28 17:13:34 -0400

    [SHARE]

--- code/job_scripts/submit_compute_recon_vigilance.sh
+++ code/job_scripts/submit_compute_recon_vigilance.sh
@@ -14,6 +14,7 @@
 #SBATCH --cpus-per-task=80
 
 ID=$(printf "%02d\n" "$SLURM_ARRAY_TASK_ID")
+module load freesurfer/7.1.0
 
 SCRATCH="$(readlink -f "$SCRATCH")"
 
@@ -21,7 +22,7 @@ PROJECT="$SCRATCH/random-matrix-fmri"
 DATA="$PROJECT/data/updated/Rest_w_VigilanceAttention"
 FILE="$DATA/ds001168-download/sub-$ID/ses-1/anat/sub-$ID""_ses-1_T1w.nii.gz"
 OUTDIR="$PROJECT/data/updated/Rest_w_VigilanceAttention/ds001168-download/sub-$ID/ses-1/anat"
-export SUBJECTS_DIR="$OUTDIR/freesurfer"
 
-module load freesurfer/7.1.0
+export SUBJECTS_DIR="$OUTDIR/freesurfer"
+cd "$DATA" || exit 1
 recon-all -subject "sub-$ID""_ses-1" -i "$FILE"
