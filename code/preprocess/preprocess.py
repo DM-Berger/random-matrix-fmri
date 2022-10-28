6ea4abbea1ccf1f5fc1c05b167c41bb186dde0d8
Derek M Berger <dmberger.dev@gmail.com>
2022-10-27 21:37:18 -0400

    [SHARE]

--- code/preprocess/preprocess.py
+++ code/preprocess/preprocess.py
@@ -260,8 +260,10 @@ class FmriScan(Loadable):
 
     def find_t1w_file(self) -> Path:
         anat_dir = self.source.parent.parent / "anat"
-        if anat_dir.parent.name == "ses-2":
-            anat_dir = anat_dir.parent.parent / "ses-1/anat"
+        if "ses-2" in str(anat_dir):
+            anat_dir = Path(str(anat_dir).replace("ses-2", "ses-1"))
+        # if anat_dir.parent.name == "ses-2":
+        #     anat_dir = anat_dir.parent.parent / "ses-1/anat"
         anat_img = sorted(anat_dir.glob("*T1w.nii.gz"))
         if len(anat_img) == 0:
             raise RuntimeError(f"Missing T1w file at {anat_dir}")
