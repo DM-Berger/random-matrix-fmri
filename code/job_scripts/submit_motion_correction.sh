ef39949d77bcc168c6f5adea7f2455fc3224abc9
Derek Berger <dberger@stfx.ca>
2022-10-28 23:39:26 -0400

    add motion correction job script

---
+++ code/job_scripts/submit_motion_correction.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=def-jlevman
+#SBATCH --mail-user=dberger@stfx.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=TIME_LIMIT_90
+#SBATCH --profile=all
+#SBATCH --job-name=motioncorr
+#SBATCH --output=motioncorr_%j.out
+#SBATCH --time=00-24:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=80
+
+
+SCRATCH="$(readlink -f "$SCRATCH")"
+PROJECT="$SCRATCH/random-matrix-fmri"
+SCRIPT="$PROJECT/code/preprocess/preprocess.py"
+source .venv_preproc/bin/activate
+python "$SCRIPT"
