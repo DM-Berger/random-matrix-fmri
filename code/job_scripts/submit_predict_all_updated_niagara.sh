ecb7abbadb8c5e6f67e7c24e2a0de0f478aa4e2d
Derek M Berger <dmberger.dev@gmail.com>
2022-10-30 14:01:04 -0400

    write job script for predict all updated

---
+++ code/job_scripts/submit_predict_all_updated_niagara.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --account=def-jlevman
+#SBATCH --mail-user=dberger@stfx.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=TIME_LIMIT_90
+#SBATCH --profile=all
+#SBATCH --job-name=predict_all
+#SBATCH --array=0-23
+#SBATCH --output=predict_all_%A_%a_%j.out
+#SBATCH --time=00-24:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=80
+
+SCRATCH="$(readlink -f "$SCRATCH")"
+PROJECT="$SCRATCH/random-matrix-fmri"
+CODE="$PROJECT/code"
+RUN_SCRIPT="$PROJECT/run_python.sh"
+
+PY_SCRIPTS="$CODE/scripts"
+PY_SCRIPT="$(readlink -f "$PY_SCRIPTS/predict_everything_array_updated.py")"
+
+bash "$RUN_SCRIPT" "$PY_SCRIPT"
