345d043a471052d7504acd04b76bb49407f2284f
Derek M Berger <dmberger.dev@gmail.com>
2022-10-30 12:10:15 -0400

    add levelvar script

---
+++ code/scripts/compute_updated_levelvars.py
@@ -0,0 +1,16 @@
+# fmt: off
+import sys  # isort: skip
+from pathlib import Path  # isort: skip
+ROOT = Path(__file__).resolve().parent.parent
+sys.path.append(str(ROOT))
+# fmt: on
+
+from rmt.enumerables import PreprocLevel, UpdatedDataset
+from rmt.updated_dataset import UpdatedProcessedDataset, levelvars
+
+if __name__ == "__main__":
+    for source in UpdatedDataset:
+        for preproc in PreprocLevel:
+            for degree in [3, 5, 7, 9]:
+                data = UpdatedProcessedDataset(source=source, preproc_level=preproc)
+                rigs = levelvars(dataset=data, degree=degree, parallel=True)
