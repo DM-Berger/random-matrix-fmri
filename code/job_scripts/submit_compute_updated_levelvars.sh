402228b29f2054f13ed57a18bb0ef47f71ce7b26
Derek M Berger <dmberger.dev@gmail.com>
2022-10-30 12:25:35 -0400

    add levelvar job script

---
+++ code/job_scripts/submit_compute_updated_levelvars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --account=def-jlevman
+#SBATCH --mail-user=dberger@stfx.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=TIME_LIMIT_90
+#SBATCH --profile=all
+#SBATCH --job-name=all_levelvar
+#SBATCH --output=all_levelvars_%j.out
+#SBATCH --time=00-08:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=80
+
+SCRATCH="$(readlink -f "$SCRATCH")"
+PROJECT="$SCRATCH/random-matrix-fmri"
+CODE="$PROJECT/code"
+RUN_SCRIPT="$PROJECT/run_python.sh"
+
+PY_SCRIPTS="$CODE/scripts"
+PY_SCRIPT="$(readlink -f "$PY_SCRIPTS/compute_updated_levelvars.py")"
+
+bash "$RUN_SCRIPT" "$PY_SCRIPT"
