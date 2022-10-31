2c7ca44d619562f8af812ef45bf4cb279f6c8a1f
Derek M Berger <dmberger.dev@gmail.com>
2022-10-31 11:34:57 -0400

    derp

--- code/scripts/compute_updated_rigidities.py
+++ code/scripts/compute_updated_rigidities.py
@@ -5,12 +5,13 @@ ROOT = Path(__file__).resolve().parent.parent
 sys.path.append(str(ROOT))
 # fmt: on
 
-from rmt.enumerables import PreprocLevel, UpdatedDataset
+from rmt.enumerables import PreprocLevel, TrimMethod, UpdatedDataset
 from rmt.updated_dataset import UpdatedProcessedDataset, rigidities
 
 if __name__ == "__main__":
     for source in UpdatedDataset:
         for preproc in PreprocLevel:
             for degree in [3, 5, 7, 9]:
-                data = UpdatedProcessedDataset(source=source, preproc_level=preproc)
-                rigs = rigidities(dataset=data, degree=degree, parallel=True)
+                for trim in [None, *TrimMethod]:
+                    data = UpdatedProcessedDataset(source=source, preproc_level=preproc)
+                    rigs = rigidities(dataset=data, degree=degree, trim_method=trim, parallel=True)
